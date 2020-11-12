9f83baf1cacc10788d7debfb1811882aee2afb35
Florian <florianbar01@gmail.com>
2020-11-12 10:08:49 +0200

    Created utility functions and setup tests

--- src/components/Congrats.test.js
+++ src/components/Congrats.test.js
@@ -2,10 +2,23 @@ import React from 'react';
 import Enzyme, { shallow } from 'enzyme';
 import EnzymeAdapter from '@wojtekmaj/enzyme-adapter-react-17';
 
+import { findByTestAttr } from '../../test/testUtils';
 import Congrats from './Congrats';
 
 Enzyme.configure({ adapter: new EnzymeAdapter() });
 
+const setup = (props = {}) => {
+    return shallow(<Congrats {...props} />);
+};
+
 test("renders without error", () => {
+    
+});
+
+test("renders no text when 'success' prop is false", () => {
+
+});
+
+test("renders non-empty congratz message when 'success' prop is true", () => {
 
 });
