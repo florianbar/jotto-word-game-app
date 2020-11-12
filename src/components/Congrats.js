39757160675d51eb4045d79e92e46e26d2346835
Florian <florianbar01@gmail.com>
2020-11-12 09:55:20 +0200

    Setup Congrats component and test

---
+++ src/components/Congrats.js
@@ -0,0 +1,14 @@
+import React from 'react';
+
+/**
+ * Functional react component for congratulatory message.
+ * @function
+ * @returns {JSX.Element} - Rendered component (or null if 'success' prop is false)
+ */
+const Congrats = ({ success }) => {
+    return (
+        <div></div>
+    );
+};
+
+export default Congrats;
