4c2a07d51a89766cc1dbbd9d75718f2a3eadfe4a
Florian <florianbar01@gmail.com>
2020-11-12 17:22:45 +0200

    Added default props

--- src/components/Congrats.test.js
+++ src/components/Congrats.test.js
@@ -7,18 +7,21 @@ import Congrats from './Congrats';
 
 Enzyme.configure({ adapter: new EnzymeAdapter() });
 
+const defaultProps = { success: false };
+
 const setup = (props = {}) => {
-    return shallow(<Congrats {...props} />);
+    const setupProps = { ...defaultProps, ...props };
+    return shallow(<Congrats {...setupProps} />);
 }; 
 
 test("renders without error", () => {
-    const wrapper = setup({ success: false });
+    const wrapper = setup();
     const component = findByTestAttr(wrapper, "component-congrats");
     expect(component.length).toBe(1);
 });
 
 test("renders no text when 'success' prop is false", () => {
-    const wrapper = setup({ success: false });
+    const wrapper = setup();
     const component = findByTestAttr(wrapper, "component-congrats");
     expect(component.text()).toBe("");
 });
