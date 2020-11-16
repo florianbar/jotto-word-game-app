1023235da7d7637be073c18e8582a547c135f746
Florian <florianbar01@gmail.com>
2020-11-16 08:50:01 +0200

    Added prop and propTypes to component

--- src/components/GuessedWords.js
+++ src/components/GuessedWords.js
@@ -1,13 +1,19 @@
 import React from 'react';
 import PropTypes from 'prop-types';
 
-const GuessedWords = (props) => {
+const GuessedWords = ({ guessedWords }) => {
     return (
         <div></div>
     );
 };
 
 GuessedWords.propTypes = {
+    guessedWords: PropTypes.arrayOf(
+        PropTypes.shape({
+            guessedWord: PropTypes.string,
+            letterMatchCount: PropTypes.number
+        })
+    ).isRequired
 };
 
 export default GuessedWords;
