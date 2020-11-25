c4c1baeba6681702c6e77eb25035b1e27a30d0c0
Florian <florianbar01@gmail.com>
2020-11-25 09:27:21 +0200

    Added secretWord action creator and reducer

--- src/store/reducers/secretWordReducer.js
+++ src/store/reducers/secretWordReducer.js
@@ -2,6 +2,8 @@ import { actionTypes } from '../actions';
 
 const secretWordReducer = (state = null, action) => {
     switch(action.type) {
+        case actionTypes.SET_SECRET_WORD:
+            return action.payload;
         default:
             return state;
     }
